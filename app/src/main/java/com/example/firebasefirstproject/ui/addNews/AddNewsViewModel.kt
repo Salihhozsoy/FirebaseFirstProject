b354f5cfe321a4faabd3ac061ad2e5b8d2a11ab5
Salih Özsoy <salihhozsoy@gmail.com>
2023-09-06 10:29:27 +0300

    multiple photo choose and news photo added

--- app/src/main/java/com/example/firebasefirstproject/ui/addNews/AddNewsViewModel.kt
+++ app/src/main/java/com/example/firebasefirstproject/ui/addNews/AddNewsViewModel.kt
@@ -7,7 +7,7 @@ import com.example.firebasefirstproject.News
 import com.example.firebasefirstproject.data.repository.NewsRepository
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,8 +15,7 @@ import javax.inject.Inject
 @HiltViewModel
 class AddNewsViewModel @Inject constructor(private val newsRepository: NewsRepository, private val firebaseAuth: FirebaseAuth):ViewModel() {
 
-    private val _newsPhotos:MutableSharedFlow<List<Uri>> = MutableSharedFlow()
-  //  val newsPhotos:MutableSharedFlow<List<Uri>> = _newsPhotos
+    private val _newsPhotos: MutableStateFlow<List<Uri>> = MutableStateFlow(emptyList())
     fun addNews(title:String,content:String){
         viewModelScope.launch {
             val news =News(title=title, content = content, editorId = firebaseAuth.currentUser?.uid)
